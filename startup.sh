c639985b065f6afd2614da25e834b65dcf209700
Unai Lopez Novoa <ulopeznovoa@users.noreply.github.com>
2022-12-05 16:27:19 +0100

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -2,4 +2,4 @@ sudo apt update
 sudo apt install -y python3-pip git
 sudo pip3 install flask
 git clone https://github.com/ulopeznovoa/AS-ejemplo-autoscale.git
-sudo python3 AS-ejemplo-autoscale/server.py
+sudo python3 AS-ejemplo-autoscale/server.py &
